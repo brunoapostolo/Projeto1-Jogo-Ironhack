77c8423f5ea34cfbb16305a9ffcced69e7a94312
brunoapostolo <53492926+brunoapostolo@users.noreply.github.com>
2022-08-24 17:49:29 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -6,12 +6,7 @@ const medio = ["78----------72---3913--4----67-----5--897-6313---5---75--2---8--
 const dificil = ["46-2----1-8---9---7-9---2-5--5-8--------327--------56--26-------4-----76---4-39--",
                  "463257891582319647719846235275684319691532784834791562926178453348925176157463928"]
 
-let timer;
-let timeRemaining;
 let lives;
-let selectedNum;
-let selectedTile;
-let disableSelect;
 
 const startBtn = document.getElementById("start-btn"); /// criei variável sendo que podia usar uma função em vez de criar
 const nivelFacil = document.getElementById("diff-1");/// criei variável sendo que podia usar uma função em vez de criar
@@ -21,6 +16,7 @@ const nivelDificil = document.getElementById("diff-3");/// criei variável sendo
 startBtn.addEventListener("click",startGame, lives=10);
 
 function startGame(){
+
     let board;
     let gabarito;
     if(nivelFacil.checked) {
@@ -35,10 +31,27 @@ function startGame(){
         board = dificil[0];
         gabarito = dificil[1];
     }
-    disableSelect=false;
     generateBoard(board,gabarito);
     id("game").classList.remove("hidden")
-    return board, gabarito
+    
+     
+    
+    
+    
+    let minutos =0;
+    if(id("time-1").checked){
+        minutos = 5;
+    }
+    else if(id("time-2").checked){
+        minutos = 7;
+    }
+    else if (id("time-3").checked) {
+        minutos = 10;
+    }
+    let time = minutos * 60
+    setTimer(time); 
+
+    return board, gabarito, time
 }
 
 function generateBoard(board,gabarito){
@@ -46,6 +59,7 @@ function generateBoard(board,gabarito){
     let idCount =0
     for (let i=0; i<81;i++){
         let tile= document.createElement("input")
+        tile.autocomplete="off"
         if (board.charAt(i)!="-"){
             tile.value = board.charAt(i);
             tile.readOnly=true;
@@ -78,7 +92,9 @@ function generateBoard(board,gabarito){
                 element.classList.add("correct");
             }    
             else if(isNaN(a) || a===" "||a.length===2){
-                alert("Digite um numero de 0 a 9")
+                alert("Digite um numero de 1 a 9")
+                element.classList.remove("correct");
+                element.classList.add("incorrect");
             }        
             else if (a.length===1 && a!==""&& a!==gabarito[element.id]){
                 element.classList.remove("correct");
@@ -87,39 +103,47 @@ function generateBoard(board,gabarito){
                 console.log("Você digitou errado");
                 id("lives").innerText= `Você ainda tem ${lives} vidas`
             }
+            if (lives===0){
+                id("lamentacao").classList.remove("hidden");
+                qsa(".tile").forEach((element)=>{
+                    element.classList.add("incorrect");
+                    element.disabled=true
+                })
+                id("lives").innerText=`Você gastou todas as suas vidas`
+                id("perdeu").innerText=`Infelizmente você perdeu o jogo, o número de vezes que você acertou foi ${qsa(".correct").length}. Para começar um novo jogo clique em "começar o seu jogo"`
+            }
+            
+            let numeroIncognitas=0   //numero de numeros que faltam acertar para completar o Sudoku
+            for (let c =0; c<board.length;c++){
+                if(board[c]==="-"){
+                    numeroIncognitas++
+                }
+            }
+
             let corretos = qsa(".correct");
             let faltamAcertar
-            if(board===facil[0]){
-                faltamAcertar=47-corretos.length;
-                console.log("faltam acertar "+faltamAcertar);
-            }
-            if(board===medio[0]){
-                faltamAcertar=51-corretos.length;
-                console.log("Faltam acertar "+ faltamAcertar);
-            }
-            else if (board===dificil[0]){
-                faltamAcertar=56-corretos.length;
-                console.log("Faltam acertar "+ faltamAcertar);
-            }
+            faltamAcertar=numeroIncognitas-corretos.length;
             if (faltamAcertar===0){
                 id("board").classList.add("hidden");
+                id("timer").innerText=`Você consegui completar o jogo de Sudoku`
+                qsa(".tile").forEach((element)=>{
+                    element.classList.add("correct")
+                    element.disabled=true
+
+                })
                 id("congratulations").classList.remove("hidden");
             }
+
+
+
+
+            return faltamAcertar;
         })
     })
 
 }
 
-function checkIfGameisDone(board){
-    let contador =0
-    for (let a=0; a<81;a++){
-        if(board[a]==="-"){
-            contador++
-        }
-    }
-    console.log(lives)
-    console.log(qsa(".correct"))
-}
+
 
 
 /// consertar essa função que é para limpar o tabuleiro toda vez que eu quiser começar um jogo novo   
@@ -130,9 +154,56 @@ function clearPrevious(){
     })
     lives =10;
     id("lives").innerText= `Você tem ${lives} vidas`
+    id("congratulations").classList.add("hidden");
+    id("lamentacao").classList.add("hidden");
+
+
+
+
+       
 
 }
 
+function setTimer(time){
+    const acabouTempo = qsa(".tile");
+    let cronometro = setInterval(()=>{
+
+        let minutes = Math.floor(time/60);
+        let seconds = time%60;
+        if (seconds<10){
+            seconds = "0"+seconds
+        }
+        if (qsa(".correct").length===81 || (qsa(".incorrect").length===81)){
+
+        }
+        else{
+            time --;
+        }
+        id("timer").innerText = `Tempo restante: ${minutes}:${seconds}`
+        id("start-btn").addEventListener("click",()=>{
+            clearInterval(cronometro);
+        })
+        if (time===-1){
+            clearInterval(cronometro);
+
+            acabouTempo.forEach((element)=>{
+                element.classList.add('incorrect')
+                element.disabled=true
+                id("timer").innerText=`Acabou o seu tempo`
+                id("lamentacao").classList.remove("hidden")
+                id("perdeu").innerText = `Você perdeu o jogo porque acabou o seu tempo`
+            })
+        }
+
+
+    },1000)
+    
+
+    
+}
+function stop(arg){
+    return clearInterval(arg);
+}
 
 
 
